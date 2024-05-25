6f9d7a208922a04a4a82fc19e64c490173148520
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2024-05-25 04:03:51 +0300

    fix google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,37 +1,36 @@
-import Document, {
-    Html,
-    Head,
-    Main,
-    NextScript,
-    DocumentProps,
-} from 'next/document';
-import Script from "next/script";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
+import Script from 'next/script';  // Ensure you import Script from 'next/script'
 
-export default class MyDocument extends Document<DocumentProps> {
-    render(): JSX.Element {
+export default class MyDocument extends Document {
+    render() {
         return (
             <Html lang="pt">
                 <Head>
                     <meta charSet="UTF-8" />
-
                     <link rel="icon" href="/favicon.ico" />
-                    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-GWS7XE70JJ"></Script>
-                    <Script id='google-analytics'>
-                        {`window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-
-                            gtag('config', 'G-GWS7XE70JJ');`}
-                    </Script>
                     <noscript>
                         <link href="./styles/aos-noscript.css" rel="stylesheet" />
                     </noscript>
-
+                    <Script
+                        src="https://www.googletagmanager.com/gtag/js?id=G-GWS7XE70JJ"
+                        strategy="afterInteractive"
+                        async
+                    />
+                    <Script
+                        id="google-analytics"
+                        strategy="afterInteractive"
+                    >
+                        {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-GWS7XE70JJ');
+                        `}
+                    </Script>
                 </Head>
-
                 <body>
                 <noscript>You need to enable JavaScript to run this app.</noscript>
-
                 <Main />
                 <NextScript />
                 </body>
